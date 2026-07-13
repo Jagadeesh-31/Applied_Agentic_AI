4d381b1515b9de466b2a0c0cf9da0392f95b9ac2
Jagadeesh-31 <22b91a6133srkrecacin@gmail.com>
2026-07-14 00:26:52 +0530

    Initial commit

---
+++ Python/.ipynb_checkpoints/day_1-checkpoint.py
@@ -0,0 +1,15 @@
+# Day-1 
+
+# Variables and Data Types
+
+name = "Jagadeesh"
+age = 22
+course = "Agentic AI"
+
+print(name)
+print(age)
+print(course)
+
+print(type(name))
+print(type(age))
+print(type(course))
